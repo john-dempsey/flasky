84381aa08a20bd73ea467ab8b575fb1884aee4dc
John Dempsey <john.dempsey@iadt.ie>
2022-03-10 17:23:11 +0000

    Chapter 3: Localization of Dates and Times with Flask-Moment

--- hello.py
+++ hello.py
@@ -3,26 +3,29 @@ from flask import request
 from flask import make_response
 from flask import redirect
 from flask import abort
-
+from flask import render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    body = '<h1>Hello World!</h1><p>Your browser is {}</p>'.format(user_agent)
-    response = make_response(body)
-    response.set_cookie('answer', '42')
-    return response
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    if name == "joe":
-        return redirect("http://www.iadt.ie")
-    
-    if name == "jim":
-        abort(404)
-        
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
